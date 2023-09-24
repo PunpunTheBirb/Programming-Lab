73900ff849f32c169aceb2dd7f8310614e2de295
PunpunTheBirb <145882381+PunpunTheBirb@users.noreply.github.com>
2023-09-23 23:43:05 -0400

    Add files via upload

---
+++ PlayerMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PLayerMovement : MonoBehaviour
+{
+
+    PlayerInput playerInput;
+
+    InputAction moveAction;
+
+    [SerializeField] float speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        moveAction = playerInput.actions.FindAction("Movement");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        Vector2 direction = moveAction.ReadValue<Vector2>();
+        transform.position += new Vector3(direction.x, 0, direction.y) * speed * Time.deltaTime;
+    }
+}
